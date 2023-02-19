69efd8e51460c3fe1c3a49159d286f6b28bcb6da
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-20 01:54:44 +0530

    Create utils.py

---
+++ plugins/utils.py
@@ -0,0 +1,23 @@
+def get_screenshot(file):
+
+    cap = cv2.VideoCapture(file)
+
+    name = "./" + \
+
+        "".join(random.choices(ascii_uppercase + digits, k=10)) + ".jpg"
+
+    total_frames = round(cap.get(cv2.CAP_PROP_FRAME_COUNT))-1
+
+    frame_num = random.randint(0, total_frames)
+
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_num-1)
+
+    res, frame = cap.read()
+
+    cv2.imwrite(name, frame)
+
+    cap.release()
+
+    # cv2.destroyAllWindows()
+
+    return name
