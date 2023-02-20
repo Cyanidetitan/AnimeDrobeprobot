54aaf57baa8bcae08ea3bb81024dbe76f40f4523
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-02-20 11:58:19 +0530

    Update auto.py

--- plugins/auto.py
+++ plugins/auto.py
@@ -2,7 +2,7 @@ import asyncio
 import base64
 import os
 import subprocess
-from Bot import bot 
+from bot import Bot 
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from helper_func import encode
