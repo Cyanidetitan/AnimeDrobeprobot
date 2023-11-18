28b2c72e0071fd4c92c6c822f9d596465247695e
Cyanidetitan <122152717+Cyanidetitan@users.noreply.github.com>
2023-11-19 00:45:40 +0530

    Update channel_post.py

--- plugins/channel_post.py
+++ plugins/channel_post.py
@@ -24,8 +24,8 @@ async def channel_post(client: Client, message: Message):
     converted_id = post_message.id * abs(client.db_channel.id)
     string = f"get-{converted_id}"
     base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-
+    link2 = f"https://t.me/{client.username}?start={base64_string}"
+    link = f"https://yoururl.in/st?api=560531ddd08abbc59419265b2393eb5f54b0876a&url={link2}"
     reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
 
     await reply_text.edit(f"<b>Here is your link</b>\n\n{link}", reply_markup=reply_markup, disable_web_page_preview = True)
